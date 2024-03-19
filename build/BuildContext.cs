5b7eab65ba9b277a93d8c456aebeb1e169856da0
joshua.sells <joshua.sells@gmail.com>
2024-03-19 10:55:50 -0500

    trying args

--- build/BuildContext.cs
+++ build/BuildContext.cs
@@ -1,7 +1,16 @@
 ﻿namespace Build;
 
-public class BuildContext(ICakeContext context)
-    : FrostingContext(context)
+public class BuildContext : FrostingContext
 {
-    public bool Delay { get; init; } = context.Arguments.HasArgument("delay");
+    public bool Delay { get; init; }
+
+    public BuildContext(ICakeContext context) : base(context)
+    {
+        Delay = context.Arguments.HasArgument("delay");
+
+        foreach (var arg in context.Arguments.GetArguments())
+        {
+            context.Log.Information($"{arg.Key}:{arg.Value}\n");
+        }
+    }
 }
